e69319e319737ef0192aab11e1c38eae39ad035f
zionsung <1122overcomer@gmail.com>
2022-03-15 15:18:02 +0800

    add success readme, and change code to fail version

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # NYCU-Software-Testing-2022-Lab_3
+[![Build Status](https://app.travis-ci.com/ZionSung/ST-2022-310605010.svg?branch=main)](https://app.travis-ci.com/ZionSung/ST-2022-310605010)
--- src/test/java/PriorityQueueTest.java
+++ src/test/java/PriorityQueueTest.java
@@ -14,7 +14,7 @@ public class PriorityQueueTest {
 
     static Stream<Arguments> streamProvider(){
         return Stream.of(
-                Arguments.of(new int[] {1, 5, 3}, new int[] {1, 3, 5}),
+                Arguments.of(new int[] {1, 5, 3}, new int[] {1, 3, 4}),
                 Arguments.of(new int[] {-3, -1, -2, 5}, new int[] {-3, -2, -1, 5}),
                 Arguments.of(new int[] {3, -2, -5, -1, 2}, new int[] {-5, -2, -1, 2, 3}),
                 Arguments.of(new int[] {-3, 1, 11, 0, 9, 3}, new int[] {-3, 0, 1, 3, 9, 11}),
